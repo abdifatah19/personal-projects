4886062fd9d43d548cb183cdd1a675680fe0290e
abdifatah19 <abdifatah16ab@gmail.com>
2023-01-09 22:04:36 -0600

    added the model class

---
+++ Clinic Application/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Clinic_Application.Models;
+
+
+namespace Clinic_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/patient")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Clinic Application/Models/Patient.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clinic_Application.Models
+{
+    public class Patient
+
+    {
+        [Required]
+        [Range(5,30, ErrorMessage =" your first name is either too long or too short")]
+        public string? Firstname { get; set; }
+        public string? LastName { get; set; }
+        [Required(ErrorMessage ="Your Age is required")]
+        public int? Age { get; set; }
+        public DateOnly? DateofBirth { get; set; }
+        public Guid? PatientId { get; set; }
+
+        [EmailAddress(ErrorMessage ="Enter a valid email Adress")]
+        public string? EmailAddress { get; set; }
+
+        public override string ToString()
+        {
+            return "$ The patient name is: {FirstName} {lastName}. The Patients is {Age} old. The Date of birth is {DateofBirth} and the patient id is {patientId}.";
+        }
+
+
+
+
+    }
+    } 
+
